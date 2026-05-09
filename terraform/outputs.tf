27c78b2b8da627981782960dad4c17e0d9e110b8
vipin <vipin.singh@artivatic.ai>
2026-05-09 15:04:36 +0530

    FIX: All files

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,9 @@
-output "server_public_ip" {
-  description = "Public IP of server"
-  value       = aws_instance.statuspulse.public_ip
-}
-
-output "server_public_dns" {
-  description = "Public DNS"
-  value       = aws_instance.statuspulse.public_dns
+output "server_elastic_ip" {
+  description = "Elastic IP of server"
+  value       = aws_eip.statuspulse_eip.public_ip
 }
 
 output "server_ssh_command" {
   description = "SSH command to connect to the server"
-  value       = "ssh -i /home/vipinkumarsingh/terra/new/terra-key-pair.pem ubuntu@${aws_instance.statuspulse.public_ip}"
+  value       = "ssh -i /home/vipinkumarsingh/terra/new/terra-key-pair ubuntu@${aws_eip.statuspulse_eip.public_ip}"
 }
